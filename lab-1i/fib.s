79d3b841ef94e6e5d38143c8abe37a0393510da1
Yuning Lin <pandaning9112@gmail.com>
2015-09-30 16:56:49 +0800

    Update 2 fib.s and Makefile
    In Makefile, I add several test to see the value.
    
    In lab-1/fib.s and lab-1i/fib.s, I finish the origin version.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -12,32 +12,21 @@ fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
 	push {r3, r4, r5, lr}
+	subs r3, r0, #0 @ r3 = r0 - 0
+	mov r4, #0 @previous
+	mov r5, #1 @result = 1
+	cmp r4, r3 @compare r4 and r3 
+	ble .L5 @goto for loop
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
+	pop {r3, r4, r5, pc}
+	@EPILOG
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
+.L5:
+	adds r0, r4, r5 @sum = result + previous
+	adds r4, r5, #0 @previous = result + 0
+	adds r5, r0, #0 @result = sum + 0
+	subs r3, r3, #1 @i--
+	cmp r3, #1      @if r3(i) > 1
+	bgt .L5 	@loop
+	pop {r3, r4, r5, pc}
 
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-	.size fibonacci, .-fibonacci
-	.end
