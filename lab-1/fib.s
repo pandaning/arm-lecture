ef71b77425678012878c5a77434ee55a1623c20f
Yuning Lin <pandaning9112@gmail.com>
2015-10-01 15:48:45 +0800

    Tail-recursive
    This is a revision for fibonacci by using tail-recursive algorithm, it
    reduce two registers(r4, r5) to one register(r3)

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -11,31 +11,29 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r4, r5, lr}
+	push {r3, lr}
+	
+    cmp r0,#0		@if term<=0
+    ble .L3			@if(term<=0) goto .L3 (return 0)
 
-	subs r4, r0, #0 		@ R4 = R0 - 0 (update flags)
-	ble .L3 			@ if(R0 <= 0) goto .L3 (which returns 0)
-	cmp r4, #1			@ Compare R4 wtih 1
-	beq .L4				@ If R4 == 1 goto .L4 (which returns 1)
+	cmp r0, #1		@if term = 1
+	beq .L4			@if(term==1) goto.L4 (return 1)
 
-	sub r0, r4, #1			@ R0 = R4 - 1
-	bl fibonacci			@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	add r5, r0, 0			@ R5 = R0
-	sub r0,r4, #2			@ R0 = R4 - 2
-	bl fibonacci			@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	adds r0,r5, r0			@ R0 = R5 + R0 (update flags)
-	pop {r4, r5, pc}		@EPILOG
-
+	sub r0, r0, #1		@term = term - 1
+	add r3, r1, r2		@r3 = val + prev
+	add r2, r1, #0		@prev = val
+	mov r1,r3		@val = r3
+	bl fibonacci		@return fib(term-1,val+prev,val)
+	pop {r3, pc}
+	@EPILOG
 	@ END CODE MODIFICATION
 .L3:
-	mov r0, #0			@ R0 = 0
-	pop {r4, r5, pc}		@ EPILOG
+	mov r0, r2			@ R0 = prev(=0)
+	pop {r3, pc}	@ EPILOG
 
 .L4:
-	mov r0, #1			@ R0 = 1
-	pop {r4, r5, pc}		@ EPILOG
+	mov r0, r1			@ R0 = val(=1)
+	pop {r3, pc}	@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
