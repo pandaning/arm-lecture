5f218159cf80613f027fbee0aedbbbe1a965a6b5
Yuning Lin <pandaning9112@gmail.com>
2015-10-01 23:08:25 +0800

    Add Clock() calculate and file output.
    The calculation of clock and FILE output were added in this version. It
    can produce a file which include number and execution time.

--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-
+#include <time.h>
 extern int fibonacci(int x);
 
 int main(int argc, char **argv)
 {
-  int number=0;
-  int result=0;
-
-  scanf("%d",&number);
-  result = fibonacci(number);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
+    int number=0;
+    int result=0;
+    int start, finish;
+    double duration;
+    FILE *outfile;
+    scanf("%d",&number);
+    start = clock();
+    result = fibonacci(number);
+    finish = clock();
+    duration = (double)(finish - start)/CLOCKS_PER_SEC;
+    printf("The fibonacci sequence at %d is: %d, %f\n", number, result, duration);
+    
+    outfile = fopen("result.txt","a");
+    fprintf(outfile, "%d\t%f\n",number, duration);
+    fclose(outfile); 
 }
-
