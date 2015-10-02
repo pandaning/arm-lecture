b71f36ffc08e30218dd8ea74cf54846bca9cdc44
Yuning Lin <pandaning9112@gmail.com>
2015-10-02 22:02:40 +0800

    Correct the code.
    I found that previous code could cause wrong answer at fibonacci(0),
    revising it by change the compare method.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,10 +13,11 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 	subs r3, r0, #0 @ r3 = r0 - 0
-	mov r4, #0 @previous
+	mov r4, #-1 @previous
 	mov r5, #1 @result = 1
-	cmp r4, r3 @compare r4 and r3 
-	ble .L5 @goto for loop
+    add r4, r4, #1 @for compare
+	cmp r3, r4 @compare r4 and r3  (if (previous < i) =>if(i>-1)
+	bgt .L5    @if(i>=0) loop
 
 	pop {r3, r4, r5, pc}
 	@EPILOG
@@ -29,20 +30,4 @@ fibonacci:
 	cmp r3, #1      @if r3(i) > 1
 	bgt .L5 	@loop
 	pop {r3, r4, r5, pc}
-
-@
-@fibonacci:
-@	@ ADD/MODIFY CODE BELOW
-@	@ PROLOG
-@	push {r3, r4, r5, lr}
-@	subs r3, r0, #0 @ i = r0 - 0
-@	mov r4, #-1 @previous = -1
-@	mov r5, #1 @result = 1
-@	add r4, r4, #1 @for compare
-@	cmp r3, r4 @compare r4 and r3  (if (previous < i) =>if(i>-1)
-@	bgt .L5    @if(i>=0) loop
-@	pop {r3, r4, r5, pc}
-@	@EPILOG
-@	@ END CODE MODIFICATION
-@
 
